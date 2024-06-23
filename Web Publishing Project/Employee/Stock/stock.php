64edf606f9164ad17484923df19836c3d324d15f
PAN ZHI XIN <1221207256@soffice.mmu.edu.my>
2024-06-23 22:50:59 +0800

    stock

--- Web Publishing Project/Employee/Stock/stock.php
+++ Web Publishing Project/Employee/Stock/stock.php
@@ -1,6 +1,5 @@
 <?php include('../../connection.php')?>
 
-<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTP-8">
@@ -9,6 +8,14 @@
         <link href="" rel="stylesheet">
         <link rel="stylesheet" href="Stock.css">
 </head>
+
+<script type="text/javascript">
+    function confirmation()
+    {
+        answer=confirm("Do you want to delete the book?");
+        return answer;
+    }
+</script>
 
 <body>
 
@@ -26,9 +33,15 @@
                 <li class="category" id="active">
                     <a href="../Stock/stock.php">STOCK</a>
                     <div class="choice" id="active">
-                        <button><a href="#guidebook" class="selections">Guidebook</a></button>
-                        <button><a href="#novel" class="selections">Novel</a></button>
-                        <button><a href="#picturebook" class="selections">Picture Book</a></button>
+                        <?php
+                            $query = "SELECT DISTINCT Category FROM booklist ORDER BY Category ASC";
+                            $result = mysqli_query($connect, $query);
+
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $category = $row['Category'];
+                                echo '<button><a href="#' . strtolower($category) . '" class="selections">' . $category . '</a></button>';
+                            }
+                        ?>
                     </div>
                 </li>
                 <li><a href="../Manage_Order/Manage_Order.php">ORDER</a></li>
@@ -39,47 +52,66 @@
     </div>
 
     
-
     <fieldset>
         <h1 class="title">LPL Book Stock</h1>
         <button class="addBook"><a href="Add Book/add book.php">New Book</a></button>
 
-        <div>   
-            <h1 class="bType" id="guidebook">GUIDE BOOK</h1>
-            <table>                
-                <thead>
-                    <tr>
-                        <th class="bImg">Book Image</th>
-                        <th class="bName">Book Name</th>
-                        <th class="bAuthor">Author</th> 
-                        <th class="bPublisher">Publisher</th>
-                        <th class="bPrice">Price (RM)</th>
-                        <th class="delBook"></th>                                   
-                    </tr>
-                </thead>
-                <?php
-                    mysqli_select_db($connect,"dwp_project");
-                    $result = mysqli_query($connect, "select * from booklist");	
-                    $count = mysqli_num_rows($result);
-                    
-                    while($row = mysqli_fetch_assoc($result))
+            <?php
+                mysqli_select_db($connect,"dwp_project");
+                $result = mysqli_query($connect, "select * from booklist ORDER BY Category");	
+                $currentType = '';
+
+                while($row = mysqli_fetch_assoc($result))
+                {		               
+                    if ($currentType != $row['Category'])
                     {
-                    
-                ?>			
+                        if ($currentType != '')
+                        {
+                            echo '</table></div>';
+                        }
+                        $currentType = $row['Category'];
+                        echo'<div>';  
+                        echo'<h1 class="bType" id="'. strtolower($currentType) . '">' . strtoupper($currentType) . '</h1>';
+                        echo'<table>';             
+                        echo'<thead>
+                            <tr>
+                                <th class="bImg">Book Image</th>
+                                <th class="bName">Book Name</th>
+                                <th class="bAuthor">Author</th> 
+                                <th class="bPublisher">Publisher</th>
+                                <th class="bPrice">Price (RM)</th>
+                                <th class="editBook"></th>
+                                <th class="delBook"></th>                                   
+                            </tr>
+                        </thead>';
+                    }
+            ?>
                 <tr>
-                    <td><img class="bImg" src="<?php echo $row["BookIMG"]; ?>"></td>
+                    <td><img class="bImg"></td>
                     <td class="bName"><?php echo $row["Book_Name"]; ?></td>
                     <td class="bAuthor"><?php echo $row["Author"]; ?></td>
                     <td class="bPublisher"><?php echo $row["Publisher"]; ?></td>
                     <td class="bPrice"><?php echo $row["Price"]; ?></td>
-                    <td><button class="delBook">-</button></td>
+                    <td class="editBook"><a href="connection.php?edit&BookID=<?php echo $row['BookID'];?>">Edit</td>
+                    <td>
+                        <button class="delBook">
+                            <a href="booklist(delete).php?del&BookID=<?php echo $row['BookID'];?>" onclick="return confirmation();">-</a>
+                        </button>
+                    </td>
                 </tr>
-                <?php
-                    }
-                ?>
+            <?php
+                }
+            ?>
             </table>
         </div>
     </fieldset>
-
-
 </body>
+</html>
+<?php
+    if (isset($_REQUEST["del"])) 
+    {
+        $BookID=$_REQUEST["BookID"];
+        mysqli_query($connect,"delete from booklist where BookID=$BookID");
+        header("Location: stock.php"); //refresh the page
+    }
+?>
