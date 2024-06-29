5f0c735ac3eb1cfc3db70dbe4786ba947577e984
PAN ZHI XIN <1221207256@soffice.mmu.edu.my>
2024-06-29 20:17:09 +0800

    contact

--- Web Publishing Project/Employee/Contact_Record/Contact_Record.php
+++ Web Publishing Project/Employee/Contact_Record/Contact_Record.php
@@ -1,188 +1,5 @@
 <?php include('../../connection.php')?>
 
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-*{
-    font-family: 'Poppins';
-}
-
-body{
-    margin: 0;
-    display: flex;
-    background:linear-gradient(25deg, #f6e7ca, #e0a456,#ed9017);
-}
-
-.selection {
-    width: 200px;
-    background-color: #3f290d;
-    color: white;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px 0px;
-    border-radius: 30px;
-    margin: 20px;
-    padding-right: 10px;
-    padding-left: 10px;
-}
-
-
-.Logo p{
-    display: flex;
-    margin: 5px 10px 5px;
-    font-weight: 500;
-    font-size: 12.5px;
-    margin-top: 25px;
-    transform: translateX(6px);
-}
-
-.Logo img{
-    padding: 10px 3px;
-    float: left;
-    display: flex;
-    height: 60px;
-}
-
-.selection .bar img {
-    width: 100px;
-    margin-bottom: 20px;
-}
-
-.selection ul {
-    list-style: none;
-    padding: 0;
-    width: 100%;
-}
-
-.selection ul li {
-    width: 180px;
-}
-
-.selection ul li a {
-    display: block;
-    padding: 15px 20px;
-    color: white;
-    text-decoration: none;
-    width: 100%;
-    box-sizing: border-box;
-}
-
-.selection ul li.active a,
-.selection ul li a:hover {
-    background-color: #ed9017;
-    color: white;
-    border-radius: 10px;
-}
-
-.selection ul li.active a:hover{
-    background-color: #ed9017;
-    color: white;
-    border-radius: 10px;
-}
-.selection ul li a:hover {
-    background-color: rgb(136, 136, 136);
-}
-
-h1
-{
-    color: #3f290d;
-    font-size: 40px;
-}
-
-fieldset
-{
-    border: transparent;
-}
-
-table
-{
-    background-color: white;
-    border-radius: 5px;
-}
-
-th
-{
-    color: #3f290d;
-    text-align: center;
-    padding: 15px;
-    background-color: #cec7c7;
-    border-radius: 5px;
-}
-
-.name
-{
-    width: 150px;
-}
-
-.tel
-{
-    width: 130px;
-}
-
-.email
-{
-    width: 320px;
-}
-
-.message
-{
-    width: 800px;
-}
-
-td
-{
-    padding: 15px;
-    font-size: 15px;
-}
-
-tbody tr
-{
-    border-radius: 5px;
-    background-color: white;
-}
-
-tbody tr:nth-child(even)
-{    
-    background-color:#e9e3e3;
-}
-
-tbody tr:hover{
-    background-color: #d0cbcb;
-}
-
-tbody tr:nth-child(even):hover{
-    background-color: #d0cbcb;
-}
-
-div.print
-{
-    margin-top: 25px;
-    padding: 20px;
-    display: flex;
-    justify-content: flex-end;
-}
-
-button
-{
-    width: 100px;
-    font-size: 15px;
-    border-radius: 3px;
-    background-color: #cec7c7;
-    border: 2px white solid;
-    font-weight: 600;
-    color: #3f290d;
-}
-
-button:hover
-{
-    font-weight: 700;
-    color: black;
-    border: 1px black solid;
-    text-transform: uppercase;
-}
-</style>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -190,6 +7,7 @@ button:hover
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <title>Emp_Home</title>
         <link href="" rel="stylesheet">
+        <link rel="stylesheet" href="Contact_Record.css"> 
         <script>
             function printRate() {
                 window.print();
@@ -219,33 +37,35 @@ button:hover
     
         <fieldset>
             <h1>Contact Record</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="name">Name</th>
-                            <th class="tel">Tel.</th>
-                            <th class="email">Email</th>
-                            <th class="message">Message</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            $query = "SELECT Username, Contact_Number, UserEmail, Message FROM contact_record ";
-                            $result = mysqli_query($connect, $query);
+                <div class="table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="name">Name</th>
+                                <th class="tel">Tel.</th>
+                                <th class="email">Email</th>
+                                <th class="message">Message</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $query = "SELECT Username, Contact_Number, UserEmail, Message FROM contact_record ";
+                                $result = mysqli_query($connect, $query);
 
-                            if ($result && mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo '<tr>';
-                                echo '<td class="name">' . $row['Username'] . '</td>';
-                                echo '<td class="tel">' . $row['Contact_Number'] . '</td>';
-                                echo '<td class="email">' . $row['UserEmail'] . '</td>';
-                                echo '<td class="message">' . $row['Message'] . '</td>';
-                                echo '</tr>';
+                                if ($result && mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo '<tr>';
+                                    echo '<td class="name">' . $row['Username'] . '</td>';
+                                    echo '<td class="tel">' . $row['Contact_Number'] . '</td>';
+                                    echo '<td class="email">' . $row['UserEmail'] . '</td>';
+                                    echo '<td class="message">' . $row['Message'] . '</td>';
+                                    echo '</tr>';
+                                }
                             }
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
     </section>
             <fieldset>
