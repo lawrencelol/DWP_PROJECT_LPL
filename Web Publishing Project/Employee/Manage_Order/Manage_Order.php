50615da0dc44f3cbf9563cd33fd3483e0107a831
LOY-YU-XUAN <1221208223@student.mmu.edu.my>
2024-07-01 16:47:03 +0800

    ...

--- Web Publishing Project/Employee/Manage_Order/Manage_Order.php
+++ Web Publishing Project/Employee/Manage_Order/Manage_Order.php
@@ -1,111 +1,94 @@
-<?php include('../../connection.php')?>
+<?php 
+include('../../connection.php');
+
+// Fetch order data from the database
+$query = "SELECT order_id, Book_Name, Price, username, email, order_date FROM orders";
+$result = mysqli_query($connect, $query);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTP-8">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <title>Emp_Order</title>
-        <link href="" rel="stylesheet">
-        <link rel="stylesheet" href="Manage_Order.css"> 
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emp_Order</title>
+    <link href="" rel="stylesheet">
+    <link rel="stylesheet" href="Manage_Order.css"> 
 
     <script>
-        function printOrder()
-        {
+        function printRate() {
             window.print();
         }
     </script>
+</head>
 
-    <body>
-        <div class="selection">
-            <div class="Logo">
-                <img src="Logo.png" />
-            </div>
-            <div class="bar">
-                <ul>
-                    <li><a href="../Employee_Home_Page/Home_Page_EMP.php">DASHBOARD</a></li>
-                    <li><a href="../Manage_Staff/Manage_Staff.php">STAFF</a></li>
-                    <li><a href="../Manage_USER/Manage_USER.php">USER</a></li>
-                    <li><a href="../Manage_Category/Category.php">CATEGORY</a></li>
-                    <li><a href="../Stock/stock.php">STOCK</a></li>
-                    <li class="active"><a href="../Manage_Order/Manage_Order.php">ORDER</a></li>
-                    <li><a href="../Rate_Review/Rate_Review.php">RATE REVIEW</a></li>
-                    <li><a href="../Contact_Record/Contact_Record.php">CONTACT</a></li>
-                    <li><a href="../../User/Landing_Page/Landing.php">Log Out</a></li>
-                </ul>
-            </div>
+<script>
+    function printOrder() {
+        window.print();
+    }
+</script>
+
+<body>
+    <div class="selection">
+        <div class="Logo">
+            <img src="Logo.png" />
         </div>
+        <div class="bar">
+            <ul>
+                <li><a href="../Employee_Home_Page/Home_Page_EMP.php">DASHBOARD</a></li>
+                <li><a href="../Manage_Staff/Manage_Staff.php">STAFF</a></li>
+                <li><a href="../Manage_USER/Manage_USER.php">USER</a></li>
+                <li><a href="../Manage_Category/Category.php">CATEGORY</a></li>
+                <li><a href="../Stock/stock.php">STOCK</a></li>
+                <li class="active"><a href="../Manage_Order/Manage_Order.php">ORDER</a></li>
+                <li><a href="../Rate_Review/Rate_Review.php">RATE REVIEW</a></li>
+                <li><a href="../Contact_Record/Contact_Record.php">CONTACT</a></li>
+                <li><a href="../../User/Landing_Page/Landing.php">Log Out</a></li>
+            </ul>
+        </div>
+    </div>
 
-        <fieldset>
-            <div class="header">
-                <h1>Order Review</h1>
-            </div>
-            <table>
-                <thead>
-                    <tr class="header">
-                        <th class="orderId">Order Id</th>
-                        <th class="bName">Book Name</th>
-                        <th class="total">Total (RM)</th>
-                        <th class="userName">Username</th>
-                        <th class="userInfo">User Details</th>
-                        <th class="oderDate">Order Date</th>
-                        <th class="oderStatus">Order Status</th>
-                        <!-- <th class="printDel"></th> -->
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="odd">
-                        <td class="orderId">0111</td>
-                        <td class="bName">100 Ways To Bake</td>
-                        <td class="total">40.00</td>
-                        <td class="userName">Mrs. Sherly</td>
-                        <td class="userInfo">Email: sherly99@gmail.com</td>
-                        <td class="orderDate">31 May 2024</td>
-                        <td class="orderStatus"><label class="completed">Completed</label></td>
-                    </tr>
-                    <tr class="even">
-                        <td class="orderId">0112</td>
-                        <td class="bName">Me And My Pet Dinosaur<br>That Thing Under My Bed</td>
-                        <td class="total">25.00</td>
-                        <td class="userName">Mr. Peter</td>
-                        <td class="userInfo">Email: peter123@hotmail.com</td>
-                        <td class="orderDate">31 May 2024</td>
-                        <td class="orderStatus"><label class="completed">Completed</label></td>
-                       
-                    </tr>
-                    <tr class="odd">
-                        <td class="orderId">0113</td>
-                        <td class="bName">Twins</td>
-                        <td class="total">22.00</td>
-                        <td class="userName">Mrs. Farah</td>
-                        <td class="userInfo">Email: farah66@gmail.com</td>
-                        <td class="orderDate">31 May 2024</td>
-                        <td class="orderStatus"><label class="completed">Completed</label></td>
-                        
-                    </tr>
-                    <tr class="even">
-                        <td class="orderId">0114</td>
-                        <td class="bName">Knit It<br>Twins<br>That Thing Under My Bed</td>
-                        <td class="total">85.00</td>
-                        <td class="userName">Boothill</td>
-                        <td class="userInfo">Email: boothill@gmail.com</td>
-                        <td class="orderDate">31 May 2024</td>
-                        <td class="orderStatus"><label class="completed">Completed</label></td>
-                        
-                    </tr>
-                    <tr class="odd">
-                        <td class="orderId">0115</td>
-                        <td class="bName">Flippy The Silly Little Fish<br>Me and My Pet Dinosaur</td>
-                        <td class="total">22.00</td>
-                        <td class="userName">Mr. Johnathan</td>
-                        <td class="userInfo">Email: johnathan@hotmail.com</td>
-                        <td class="orderDate">31 May 2024</td>
-                        <td class="orderStatus"><label class="processing">Processing</label></td>
-                        
-                    </tr>
-                </tbody>
-            </table>
-        </fieldset>
-    </body>
+    <fieldset>
+        <div class="header">
+            <h1>Order Review</h1>
+        </div>
+        <section class="table">
+        <table>
+            <thead>
+                <tr class="header">
+                    <th class="orderId">Order Id</th>
+                    <th class="bName">Book Name</th>
+                    <th class="total">Total (RM)</th>
+                    <th class="userName">Username</th>
+                    <th class="userInfo">Email</th>
+                    <th class="orderDate">Order Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($result && mysqli_num_rows($result) > 0) {
+                    $row_class = 'odd';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr class='{$row_class}'>";
+                        echo "<td class='orderId'>{$row['order_id']}</td>";
+                        echo "<td class='bName'>{$row['Book_Name']}</td>";
+                        echo "<td class='total'>{$row['Price']}</td>";
+                        echo "<td class='userName'>{$row['username']}</td>";
+                        echo "<td class='userInfo'>Email: {$row['email']}</td>";
+                        echo "<td class='orderDate'>{$row['order_date']}</td>";
+                        echo "</tr>";
+
+                        // Alternate row class
+                        $row_class = ($row_class == 'odd') ? 'even' : 'odd';
+                    }
+                } else {
+                    echo "<tr><td colspan='7'>No orders found</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        </section>
+        <button class="print" onclick="printRate()">Print</button>
+    </fieldset>
+</body>
 </html>
